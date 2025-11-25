0635bec6c68b38dea036dc896ae5cafef6932be5
HarunMeragaSerra <meragaserraharun@gmail.com>
2025-11-25 21:33:09 +0100

    serie-card component created

--- src/app/pages/series-list/series-list.component.ts
+++ src/app/pages/series-list/series-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Series } from '../../models/series.model';
 import { SeriesService } from '../../services/series.service';
+import { TranslateService, TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-series-list',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './series-list.component.html',
   styleUrl: './series-list.component.css'
 })
@@ -13,7 +14,7 @@ export class SeriesListComponent implements OnInit{
 
   series: Series[] = [];
 
-  constructor(private seriesService: SeriesService) {}
+  constructor(private seriesService: SeriesService, private translate: TranslateService) {}
 
   ngOnInit(): void {
     this.seriesService.getAll().subscribe(data => {
