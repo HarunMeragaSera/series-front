da1ad92214bdfc94c0f8fbfa084608384c97f73f
HarunMeragaSerra <meragaserraharun@gmail.com>
2026-01-23 16:43:40 +0100

    reactive forms and form

--- src/app/pages/series-create/series-create.component.ts
+++ src/app/pages/series-create/series-create.component.ts
@@ -1,21 +1,38 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { SeriesService } from '../../services/series.service';
+import { Rating } from '../../enums/rating.enum';
 
 @Component({
   selector: 'app-series-create',
   standalone: true,
-  imports: [TranslateModule,CommonModule],
+  imports: [TranslateModule,CommonModule,ReactiveFormsModule],
   templateUrl: './series-create.component.html',
   styleUrl: './series-create.component.css'
 })
-export class SeriesCreateComponent {
+export class SeriesCreateComponent implements OnInit {
+
+  //Create genre service
+  //Import genres
+  
 
   constructor(
     private seriesService: SeriesService,
-    private route: ActivatedRoute
   ) { }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  seriesForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.maxLength(200)]),
+      rating: new FormControl<Rating | null>(null),
+      imageUrl: new FormControl('', [Validators.maxLength(500)]),
+      yearWatch: new FormControl<number | null>(null),
+      description: new FormControl('', [Validators.maxLength(2000)]),
+      genreIds: new FormControl<number[]|null>(null)
+  });
 
 }
