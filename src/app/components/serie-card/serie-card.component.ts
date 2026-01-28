88dc20f8ad49cff0f031f9e33e95bda18839d949
HarunMeragaSerra <meragaserraharun@gmail.com>
2026-01-28 17:24:00 +0100

    button card styling

--- src/app/components/serie-card/serie-card.component.ts
+++ src/app/components/serie-card/serie-card.component.ts
@@ -2,11 +2,13 @@ import { Component, Input } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { Series } from '../../models/series.model';
 import { Router } from '@angular/router';
+import { Rating } from '../../enums/rating.enum';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'series-card',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './serie-card.component.html',
   styleUrl: './serie-card.component.css'
 })
@@ -15,7 +17,7 @@ export class SerieCardComponent {
 
   @Input() serie!: Series;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   onImageError(event: Event) {
     (event.target as HTMLImageElement).src = 'assets/default.png';
@@ -24,4 +26,18 @@ export class SerieCardComponent {
   goToDetail() {
     this.router.navigate(['/series', this.serie.publicId]);
   }
+
+  ratingClassMap: Record<Rating, string> = {
+    [Rating.insufficient]: 'rating-insufficient',
+    [Rating.sufficient]: 'rating-sufficient',
+    [Rating.notable]: 'rating-notable',
+    [Rating.excellent]: 'rating-excellent',
+  };
+
+  ratingSoftClassMap: Record<Rating, string> = {
+    [Rating.insufficient]: 'year-soft-red',
+    [Rating.sufficient]: 'year-soft-yellow',
+    [Rating.notable]: 'year-soft-blue',
+    [Rating.excellent]: 'year-soft-green',
+  };
 }
