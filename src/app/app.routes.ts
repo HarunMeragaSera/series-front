77be99da56ee19865b5480e27e96bbcffbca12fc
HarunMeragaSerra <meragaserraharun@gmail.com>
2026-02-04 16:50:41 +0100

    listsComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { SeriesListComponent } from './pages/series-list/series-list.component';
 import { SeriesDetailComponent } from './pages/series-detail/series-detail.component';
 import { SeriesCreateComponent } from './pages/series-create/series-create.component';
 import { SeriesUpdateComponent } from './pages/series-update/series-update.component';
+import { ListsComponent } from './pages/lists/lists.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'series', pathMatch: 'full' },
   { path: 'series', component: SeriesListComponent },
   {path: 'series/create', component: SeriesCreateComponent},
   {path: 'series/update/:publicId', component: SeriesUpdateComponent},
-  {path: 'series/:publicId', component: SeriesDetailComponent}
+  {path: 'series/:publicId', component: SeriesDetailComponent},
+  {path:'lists', component: ListsComponent}
 ];
