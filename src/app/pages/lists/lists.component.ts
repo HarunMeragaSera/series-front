22771f9c38cf0a65d297858d7c879c344ad5c6d3
HarunMeragaSerra <meragaserraharun@gmail.com>
2026-02-04 17:49:17 +0100

    lists appearance

--- src/app/pages/lists/lists.component.ts
+++ src/app/pages/lists/lists.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SeriesListService } from '../../services/series_list.service';
+import { Router } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { SeriesListDTO } from '../../models/series_list.model';
 
 @Component({
   selector: 'app-lists',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './lists.component.html',
   styleUrl: './lists.component.css'
 })
-export class ListsComponent {
+export class ListsComponent implements OnInit {
+
+  lists: SeriesListDTO[] = [];
+
+  constructor(
+    private listService: SeriesListService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.getLists();
+  }
+
+  getLists() {
+    this.listService.getAll().subscribe(data => {
+      this.lists = data;
+      console.log(this.lists);
+    });
+  }
 
 }
